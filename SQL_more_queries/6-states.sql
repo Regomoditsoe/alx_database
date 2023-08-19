f8a4d5d1bee48c5cfd4a38454995a82cda20e3d0
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-19 10:13:57 -0700

    States table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Create the database hbtn_0d_usa and the table states.
+-- Create a database.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+--- Use database.
+USE hbtn_0d_usa;
+--Create a table.
+CREATE TABLE IF NOT EXISTS states(id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
