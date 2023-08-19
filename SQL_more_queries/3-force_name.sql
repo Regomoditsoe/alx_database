b1dafda107ee7b223daa9f20f4beb344dfbbdb34
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-19 09:31:37 -0700

    Always a name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- Creates the table force_name  on my MySQL server
 -- Create table
-CREATE TABLE IF EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT  EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
