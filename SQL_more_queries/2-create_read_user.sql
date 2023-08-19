c0c8738ea5eeea30543f144d403e0cbe2e49f42b
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-19 08:50:05 -0700

    Create user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,10 @@
 -- Create a database hbtn_0d_2 and the user user_0d_2.
+
 -- Create a database
-CREATE DATABASE IF NOT EXISTS htbn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
 -- Creates a user
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
 -- Grants SELECT privileges to a user 
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
