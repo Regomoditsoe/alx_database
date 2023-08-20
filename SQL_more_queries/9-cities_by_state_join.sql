5b499cd221d078dd6b2fbdb827c3db9c0dfb9807
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-20 07:41:57 -0700

    Cities by states

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- List all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.states_id=states_id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states_id ORDER BY cities.id;
