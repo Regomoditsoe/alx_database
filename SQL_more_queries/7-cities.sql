269574f8647eac02c3f5bae73d263a334223eca7
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-20 07:22:34 -0700

    Add Cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
 -- Create the database hbtn_0d_usa in the table cities of my MySQL server.
 
--- Create a database
+-- Create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Use a database
+-- Use database
 USE hbtn_0d_usa;
 
--- Create a table
- CREATE A TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id),  FOREIGN KEY(state_id) REFERENCES states(id));
+-- Create table named 'cities'
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id), name VARCHAR(256) NOT NULL);
